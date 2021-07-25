ef36a0c73fcccf422364328f7a6d0170d20074f3
Rafael Santos <rafael_16001@hotmail.com>
2021-07-25 12:19:54 -0300

    H2 dabase, test profile, JPA

---
+++ src/test/java/com/rafaelanges/system/SystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelanges.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
